2cd5b325fd4e664bcfa504071b786bb927aeffe6
abhinav85 <rajputabhinav85@gmail.com>
2019-02-11 21:53:52 +0530

    Made some changes to Kadane's Implementation

--- Array/kadaneArray.js
+++ Array/kadaneArray.js
@@ -1,21 +1,19 @@
-module.exports = { 
-    //param A : array of integers
-    //return an integer
-       maxSubArray : function(A){
-           var sumMax = -1000000;
-           var sumTillHere = 0;
-          // console.log(sumMax);
-           for(var i = 0 ; i < A.length ; i++){
-               sumTillHere = sumTillHere + A[i];
-               if(sumTillHere > sumMax){
-                   sumMax = sumTillHere;
-               }
-               
-               if(sumTillHere <= 0){
-                   sumTillHere = 0
-               }
-           }
-           return sumMax;
-       }
-   };
+
+function maxSubArray(A){
+    var sumMax = -1000000;
+    var sumTillHere = 0;
+    for(var i = 0 ; i < A.length ; i++){
+        sumTillHere = sumTillHere + A[i];
+        if(sumTillHere > sumMax){
+            sumMax = sumTillHere;
+        }
+        
+        if(sumTillHere <= 0){
+            sumTillHere = 0
+        }
+    }
+    return sumMax;
+}
+
+console.log(maxSubArray([1,2,-3,4,5,6, -2 ,-4 ,5 ]));
    
